badef0e11af56fb7a6efd3394931b21da7e4e9d5
Ashiv Gupta <ashiv.gupta@spinny.com>
2020-09-22 15:20:52 +0530

    Added penellum

--- examples/react/src/App.js
+++ examples/react/src/App.js
@@ -1,15 +1,29 @@
 import React, { useRef } from 'react'
-import { ThreeSixtyViewer } from 'react-threesixty'
+import { ThreeSixtyViewer } from 'react-threesixty';
+import { Pannellum } from "pannellum-react";
 
 const App = () => {
     const imageArr = [...Array(35)].map((ite, index) => {
         return `https://spinny-images.s3.ap-south-1.amazonaws.com/temp-assets/nexon-360/${index + 1}.jpg`;
     });
+    const panoramaImage = 'https://spinny-images.s3.ap-south-1.amazonaws.com/temp-assets/nexon-360/interior.jpg'
     const viewerRef = useRef(null);
 
     return (
         <React.Fragment>
             <ThreeSixtyViewer image={imageArr} viewerRef={viewerRef} width='auto' height='600' />
+            {/* <Pannellum
+                width="100%"
+                height="500px"
+                image={panoramaImage}
+                pitch={10}
+                yaw={180}
+                hfov={110}
+                autoLoad
+                onLoad={() => {
+                    console.log("panorama loaded");
+                }}
+            /> */}
         </React.Fragment>
     )
 }
