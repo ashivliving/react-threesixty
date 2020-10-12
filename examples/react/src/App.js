8e8bae4acee539b45d4acce48501deefdcf844c8
Ashiv Gupta <ashiv.gupta@spinny.com>
2020-10-12 11:34:05 +0530

    Updated interior image

--- examples/react/src/App.js
+++ examples/react/src/App.js
@@ -6,12 +6,12 @@ const App = () => {
     const imageArr = [...Array(35)].map((ite, index) => {
         return `https://spinny-images.s3.ap-south-1.amazonaws.com/temp-assets/nexon-360/${index + 1}.jpg`;
     });
-    const panoramaImage = 'https://spinny-images.s3.ap-south-1.amazonaws.com/temp-assets/nexon-360/interior.jpg'
+    const panoramaImage = 'https://spinny-images.s3.ap-south-1.amazonaws.com/temp-assets/nexon-360/interior-temp.jpg'
     const viewerRef = useRef(null);
 
     return (
         <React.Fragment>
-            <ThreeSixtyViewer image={imageArr} viewerRef={viewerRef} width='auto' height='600' />
+            <ThreeSixtyViewer image={imageArr} width='auto' height='600' />
             {/* <Pannellum
                 width="100%"
                 height="500px"
