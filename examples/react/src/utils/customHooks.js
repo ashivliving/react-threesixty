619f4b4fc388a1b045301049eb6886469b20bef5
Jay Patel <jay.patel@spinny.com>
2021-07-09 17:47:49 +0530

    fixes

--- examples/react/src/utils/customHooks.js
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-
-export const useStyle = cssString => {
-  useEffect(() => {
-    const style = document.createElement('style');
-    style.appendChild(document.createTextNode(cssString));
-    document.head.appendChild(style);
-  }, [cssString])
-}
